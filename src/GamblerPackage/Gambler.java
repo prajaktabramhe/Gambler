8efaad6b055ac620e323906e8d80fba65c5b46bc
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-11-23 17:24:12 +0530

    refactor the code of calculating the 50% stake

--- src/GamblerPackage/Gambler.java
+++ src/GamblerPackage/Gambler.java
@@ -1,4 +1,5 @@
 package GamblerPackage;
+import java.util.Random;
 public class Gambler
 {
 	public static int EveryDayStake = 100, EveryGame = 1;
@@ -6,18 +7,41 @@ public class Gambler
 	{
 		EveryDayStake=EveryDayStake-1;
 	}
-	public void resign(int i)
+	public void calculateresign()
 	{
 		double stakepercentage;
-		stakepercentage = i - (i * 0.5);
-		System.out.println("50% of the stakes are:"+stakepercentage);
+		Random rand=new Random();
+		while (true)
+		{
+			int temp = rand.nextInt(2);
+			System.out.println(temp);
+			if(temp==1)
+			{
+				EveryDayStake=EveryDayStake+1;
+			}
+			else
+			{
+				EveryDayStake=EveryDayStake-1;
+			}
+			
+			if(EveryDayStake==150)
+			{
+				System.out.println(EveryDayStake+":won done for the day");
+				break;
+			}
+			else if(EveryDayStake==50)
+			{
+				System.out.println(EveryDayStake+":lose done for the day");
+				break;
+			}
+		}
 	}
 	public static void main(String[] args)
 	{
 		
 		Gambler person=new Gambler();
 		person.bet();
-		person.resign(100);
+		person.calculateresign();
 	}
 
 }
