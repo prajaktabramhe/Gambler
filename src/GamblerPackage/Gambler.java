02a4e9923328a8636fce51865ba4849647661b79
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-11-23 16:33:16 +0530

    calculating 50% of stake

--- src/GamblerPackage/Gambler.java
+++ src/GamblerPackage/Gambler.java
@@ -1,11 +1,23 @@
 package GamblerPackage;
 public class Gambler
 {
-	public static final int EveryDayStake = 100, EveryGame = 1;
+	public static int EveryDayStake = 100, EveryGame = 1;
+	public void bet()
+	{
+		EveryDayStake=EveryDayStake-1;
+	}
+	public void resign(int i)
+	{
+		double stakepercentage;
+		stakepercentage = i - (i * 0.5);
+		System.out.println("50% of the stakes are:"+stakepercentage);
+	}
 	public static void main(String[] args)
 	{
 		
-		
+		Gambler person=new Gambler();
+		person.bet();
+		person.resign(100);
 	}
 
 }
