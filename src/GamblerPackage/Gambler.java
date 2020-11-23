c976d7c0073806a6bd4117fd159d9738f5c62b02
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-11-23 16:11:49 +0530

    make the bet of

--- src/GamblerPackage/Gambler.java
+++ src/GamblerPackage/Gambler.java
@@ -1,11 +1,17 @@
 package GamblerPackage;
 public class Gambler
 {
-	public static final int EveryDayStake = 100, EveryGame = 1;
+	public static int EveryDayStake = 100, EveryGame = 1;
+	public void bet()
+	{
+		EveryDayStake=EveryDayStake-1;
+	}
+	
 	public static void main(String[] args)
 	{
 		
-		
+		Gambler person=new Gambler();
+		person.bet();
 	}
 
 }
